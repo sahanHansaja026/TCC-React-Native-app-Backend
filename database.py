fe3347f2a33f53e5e4e96ea4729e588e579a56f9
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-25 00:01:14 +0530

    change database

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker # type: ignore
 # Database connection URL format:
 # postgresql://<username>:<password>@<host>:<port>/<database_name>
 # postgresql://postgres:123@localhost:5432/test_fastapi_backend
-DATABASE_URL = "postgresql://postgres:123@localhost:5432/test_fastapi_backend"
+DATABASE_URL = "postgresql://neondb_owner:npg_Nh94KGFseVTC@ep-sparkling-lake-ad9zpxqy-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 # 'postgresql://neondb_owner:npg_Nh94KGFseVTC@ep-sparkling-lake-ad9zpxqy-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
 
 engine = create_engine(DATABASE_URL)
