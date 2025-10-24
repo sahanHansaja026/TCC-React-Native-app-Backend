0c2e334434ae6f216ae092fd4b52eff26839aaf3
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-25 00:59:53 +0530

    change some things

--- auth.py
+++ auth.py
@@ -1,12 +1,9 @@
-from passlib.context import CryptContext  # type: ignore
+from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 def hash_password(password: str):
-    # truncate password to 72 characters
-    password = password[:72]
     return pwd_context.hash(password)
 
 def veryfy_password(plain_password: str, hashed_password: str):
-    # truncate password to 72 characters before verifying
-    return pwd_context.verify(plain_password[:72], hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
