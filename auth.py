9389e862cde1165ca03dfb7dbdc2857920bfc5d3
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-25 08:59:27 +0530

    change library

--- auth.py
+++ auth.py
@@ -1,9 +1,9 @@
-from passlib.context import CryptContext
+from passlib.context import CryptContext  # type: ignore
 
-pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
     return pwd_context.hash(password)
 
-def veryfy_password(plain_password: str, hashed_password: str):
+def verify_password(plain_password: str, hashed_password: str):
     return pwd_context.verify(plain_password, hashed_password)
