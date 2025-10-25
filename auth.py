77cd9102d51ad0e90e89c54805fe3c4c726ca1f6
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-25 09:19:37 +0530

    fix bug 3

--- auth.py
+++ auth.py
@@ -1,12 +1,9 @@
 from passlib.context import CryptContext
 
-# Use Argon2 instead of bcrypt
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
-def hash_password(password: str):
-    """Hash the password using Argon2"""
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def veryfy_password(plain_password: str, hash_password: str):
-    """Verify the password using Argon2"""
-    return pwd_context.verify(plain_password, hash_password)
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
