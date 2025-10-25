826ff5639fcf3dd6da3a3ae298f41e22a8a52537
Sahan Hansaja <165319843+sahanHansaja026@users.noreply.github.com>
2025-10-25 09:05:08 +0530

    fix bug 1

--- auth.py
+++ auth.py
@@ -5,5 +5,5 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
+def veryfy_password(plain_password: str, hash_password: str):
+    return pwd_context.verify(plain_password, hash_password)
